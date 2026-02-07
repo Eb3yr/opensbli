f893ff173a793c75fa06218c96faf69bff5db66e
Eb3yr <Eb3yr123@gmail.com>
2026-02-07 01:12:30 +0000

    Re-arranged printed includes in opsc.py
    Re-arranged printed includes so that "OPS_%dD" prints prior to "ops_lib_core.h" and "constants.h" prints after "ops_lib_core.h".
    
    If this order is broken then "ACC<>" can be compiled with missing members which are conditionally compiled depending on "OPS_1D" through 4D being defined, and "constants.h" can have the typedef for "half" be undefined, causing compilation failure when using "SimulationDataType.set_datatype(Half)".
    
    When targeting CPU platforms, c++23 or greater must be targeted to take advantage of c++23's _Float16 type. For prior standards, OPS uses uint16_t. By default, running:
    
    make opensbli_*
    
    where * can be mpi, openmp, cuda etc, will use c++11. This can be overriden with:
    
    make opensbli_* CXXFLAGS="-O3 -fPIC -Wall -ffloat-store -g $(CXXFLAGS) -std=c++23"
    
    This will emulate float16 behaviour and will be slower than natively-supported floating point operations For CPUs supporting the AVX-512-FP16 instruction set extensions, such as Intel Xeon, native instructions for FP16 will only be used by the GCC compiler when adding the compiler flag "-mavx512fp16".
    
    Additional arguments are specified because they are ordinarily used and otherwise lost when only specifying "-std=c++23", generating worse code. Most important is the optimisation level "-O3".

--- opensbli/code_generation/opsc.py
+++ opensbli/code_generation/opsc.py
@@ -832,7 +832,7 @@ class OPSC(object):
 
     def before_main(self, algorithm):
         """ Adds the required preamble to the main opensbli.cpp file and declares the simulation constants."""
-        out = ['#include <stdlib.h> \n#include <string.h> \n#include <math.h> \n#include "constants.h"' ]
+        out = ['#include <stdlib.h> \n#include <string.h> \n#include <math.h>' ]
         from opensbli.core.kernel import ConstantsToDeclare
         # Declare a restart flag and loop variables globally
         constant_declarations = ["%s %s;" % ('int', 'restart')]
@@ -858,6 +858,7 @@ class OPSC(object):
         out += ['#define OPS_%dD' % algorithm.block_descriptions[0].ndim]
         if self.OPS_V2:
             out += ['#define OPS_API 2']
+        out += ['#include "ops_lib_core.h"', '#include "constants.h"']
         out += ['#include \"ops_seq.h\"']
         for b in algorithm.block_descriptions:
             out += ['#include \"%s_kernels.h\"' % b.block_name]
